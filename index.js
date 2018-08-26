3ca4970cfab275bf853e2f64c05bdb45baa8dc7f
TeraProxy <28067642+TeraProxy@users.noreply.github.com>
2018-08-26 23:27:07 +0200

    update for require fix

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-// Version 1.2.3
+// Version 1.2.5
 
-const Command = require('command')
+module.exports = function afker(mod) {
 
-module.exports = function afker(dispatch) {
 	let enabled = true,
 		lasttimemoved = Date.now()
 
-	dispatch.hook('C_PLAYER_LOCATION', 5, event => {
+	mod.hook('C_PLAYER_LOCATION', 5, event => {
 		if([0,1,5,6].indexOf(event.type) > -1) // running / walking / jumping / jumping (steep terrain)
 			lasttimemoved = Date.now()
 	})
 
-	dispatch.hook('C_RETURN_TO_LOBBY', 'raw', () => {
+	mod.hook('C_RETURN_TO_LOBBY', 'raw', () => {
 		if (enabled && Date.now() - lasttimemoved >= 3600000) return false // Prevents you from being logged out after not moving for 1 hour
 	})
 
@@ -19,10 +18,9 @@ module.exports = function afker(dispatch) {
 	// ### Chat Hook ### //
 	// ################# //
 
-	const command = Command(dispatch)
-	command.add('afk', () => {
+	mod.command.add('afk', () => {
 		enabled = !enabled
-		command.message('[AFKer] ' + (enabled ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'))
+		mod.command.message((enabled ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'))
 		console.log('[AFKer] ' + (enabled ? 'enabled' : 'disabled'))
 	})
 }
